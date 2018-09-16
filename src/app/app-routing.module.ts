3b29d6159ee40e3b318c4264b4334226aedceacb
dan2k <sangtong@gmail.com>
2018-09-16 16:35:19 +0700

    นำ Lazy loading เข้าใช้งานจริง

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,9 +14,19 @@ const routes: Routes = [
     loadChildren: "./receiverepair/receiverepair.module#ReceiverepairModule"
   },
   { path: "borrow", loadChildren: "./borrow/borrow.module#BorrowModule" },
-  { path: "sendrepair", loadChildren: "./sendrepair/sendrepair.module#SendrepairModule" },
-  { path: "sendreturn", loadChildren: "./sendreturn/sendreturn.module#SendreturnModule" },
-  { path: "sendreturn-but-no-receive", loadChildren: "./sendreturn-but-no-receive/sendreturn-but-no-receive.module#SendreturnButNoReceiveModule" },
+  {
+    path: "sendrepair",
+    loadChildren: "./sendrepair/sendrepair.module#SendrepairModule"
+  },
+  {
+    path: "sendreturn",
+    loadChildren: "./sendreturn/sendreturn.module#SendreturnModule"
+  },
+  {
+    path: "sendreturn-but-no-receive",
+    loadChildren:
+      "./sendreturn-but-no-receive/sendreturn-but-no-receive.module#SendreturnButNoReceiveModule"
+  },
   { path: "**", redirectTo: "listalert", pathMatch: "full" },
   { path: "", redirectTo: "listalert", pathMatch: "full" }
 ];
